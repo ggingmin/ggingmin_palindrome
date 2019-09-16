199aaede2d2af0af7fedb89a192be2d472dd103f
ggingmin <ggingmin@gmail.com>
2019-09-17 00:38:13 +0900

    Finish working

--- lib/ggingmin_palindrome.rb
+++ lib/ggingmin_palindrome.rb
@@ -1,6 +1,32 @@
 require "ggingmin_palindrome/version"
 
-module GgingminPalindrome
-  class Error < StandardError; end
-  # Your code goes here...
+class String
+
+  def palindrome?
+    processed_content == processed_content.reverse
+  end
+
+
+  # def letters
+  #   # the_letters = []
+  #   # letter_regex = /[a-z]/i
+  #   # for i in 0..self.length - 1 do
+  #   #   character = self[i]
+  #   #   if character.match(letter_regex)
+  #   #     the_letters << character
+  #   #   end
+  #   # end
+  #   # the_letters.join
+
+
+  #   self.chars.select { |c| c.match(/[a-z]/i) }.join
+    
+  # end
+
+
+  private
+
+  def processed_content
+    self.scan(/[a-z]/i).join.downcase
+  end
 end
