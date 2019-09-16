afe02149e2f0da67564407f497aacdd49263db5f
ggingmin <ggingmin@gmail.com>
2019-09-16 10:34:25 +0900

    Initialize repository

---
+++ lib/ggingmin_palindrome.rb
@@ -0,0 +1,6 @@
+require "ggingmin_palindrome/version"
+
+module GgingminPalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/ggingmin_palindrome/version.rb
@@ -0,0 +1,3 @@
+module GgingminPalindrome
+  VERSION = "0.1.0"
+end
